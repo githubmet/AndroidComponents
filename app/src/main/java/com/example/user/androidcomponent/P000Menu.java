c9b69e79c3229a89ef9755b62131b08e08e5f3d5
Hikmet Anıl <iyibidunya@gmail.com>
2018-10-23 17:04:28 +0300

    Add all android files to first AndroidComponents master branch

---
+++ app/src/main/java/com/example/user/androidcomponent/P000Menu.java
@@ -0,0 +1,13 @@
+package com.example.user.androidcomponent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class P000Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.p000menu);
+    }
+}
